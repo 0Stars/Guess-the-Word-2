dbb06bf0bda43ef2a18028cc28c36e4c835b8f57
Code-studio456 <79489448+Code-studio456@users.noreply.github.com>
2021-05-18 18:12:22 -0400

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -61,7 +61,7 @@ function check()
 		    document.getElementById("player1_score").innerHTML = player1_score;
 		}
 		else 
-		{
+		{										
 			player2_score = player2_score +1;
 		    document.getElementById("player2_score").innerHTML = player2_score;
 		}
